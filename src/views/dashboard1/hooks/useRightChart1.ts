6fdf90aa4c39beeacd35021f38d323522da70004
A PEI <146091931+yingpeisun@users.noreply.github.com>
2023-11-03 19:36:54 +0800

    Update useRightChart1.ts

--- src/views/dashboard1/hooks/useRightChart1.ts
+++ src/views/dashboard1/hooks/useRightChart1.ts
@@ -1,42 +1,62 @@
 import { onBeforeUnmount, ref } from "vue";
 import { EChartsOption } from "echarts";
-import { generateNumbers } from "@/utils";
+import axios from "axios";
+
+function increaseDate(dateString: string, days: number) {
+  const date = new Date(dateString);
+  date.setDate(date.getDate() + days);
+
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  return `${year}/${month}/${day}`;
+}
 
 export default function () {
+  let date = "2022/1/1";
   const option = ref<EChartsOption>({});
 
   function refresh() {
-    option.value = {
-      radar: {
-        indicator: [
-          { name: "用户满意度", max: 200 },
-          { name: "价格", max: 200 },
-          { name: "品质", max: 200 },
-          { name: "客户支持", max: 200 },
-          { name: "可持续性", max: 200 },
-          { name: "效果", max: 200 },
-        ],
-        radius: 100,
-      },
-      series: [
-        {
-          type: "radar",
-          data: [
-            {
-              value: generateNumbers(1, 200, 6),
-              areaStyle: {},
+    axios
+      .get(`http://35.178.189.142:8080/power-consumption?date=${date}`)
+      .then(({ data }) => {
+        option.value = {
+          tooltip: {
+            trigger: "axis",
+            axisPointer: {
+              type: "shadow",
             },
-          ],
-        },
-      ],
-    };
+          },
+          xAxis: {
+            type: "category",
+          },
+          yAxis: {
+            type: "value",
+          },
+          grid: {
+            left: "12%",
+          },
+          series: [],
+        };
+        const series: any[] = [];
+        Object.keys(data).forEach((key) => {
+          series?.push({
+            name: key,
+            data: data[key],
+            type: "line",
+          });
+        });
+        option.value.series = series;
+      });
+    date = increaseDate(date, 1);
   }
 
   refresh();
 
   const timer = setInterval(() => {
     refresh();
-  }, 3000);
+  }, 3000 * 10);
 
   onBeforeUnmount(() => {
     clearInterval(timer);
